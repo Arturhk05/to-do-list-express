c2d68b9b59c23df3a5dca5f3d4ebffc1c8556b8f
Artur Krauspenhar <arturkrauspenhar@hotmail.com>
2024-02-22 20:40:32 -0300

    middlewares

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+const log = (req, res, next) => {
+  console.log(req.body)
+  console.log(Date.now())
+  next()
+}
+
+app.use(log)
+
+app.get("/", (req, res) => {
+  res.send("<h1>Minha lista de tarefas</h1>")
+})
+
+app.get("/json", (req, res) => {
+  console.log(req.body)
+  res.json({ title: "Tarefa X", done: true })
+})
+
+app.listen(3000, () => {
+  console.log("Servidor foi iniciado.")
+})
