93f73e98a51e4b7226ed93f90551695cbef50c9c
Artur Krauspenhar <arturkrauspenhar@hotmail.com>
2024-02-22 21:14:55 -0300

    routes put and delete

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -12,4 +12,19 @@ router.post("/", (req, res) => {
   res.status(200).send(req.body)
 })
 
+router.get("/:id", (req, res) => {
+  console.log(req.params.id)
+  res.send(`ID: ${req.params.id}`)
+})
+
+router.put("/:id", (req, res) => {
+  console.log(req.params.id)
+  res.send(`PUT ID: ${req.params.id}`)
+})
+
+router.delete("/:id", (req, res) => {
+  console.log(req.params.id)
+  res.send(`DELETE ID: ${req.params.id}`)
+})
+
 module.exports = router
