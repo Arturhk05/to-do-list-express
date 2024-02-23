a7648ebf91ece744c6000e004243d6230641c800
Artur Krauspenhar <arturkrauspenhar@hotmail.com>
2024-02-23 12:01:37 -0300

    mongoose and ejs

---
+++ config/database.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose")
+mongoose.Promise = global.Promise
+
+mongoose.connect("mongodb://localhost/todo-list")
+  .then(() => console.log("Conectado ao MongoDB")) 
+  .catch((err) => console.error(err))
