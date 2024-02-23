4ac999121c7b98b1237e6601dbc63cb4f7b21844
Artur Krauspenhar <arturkrauspenhar@hotmail.com>
2024-02-23 15:19:38 -0300

    bulma

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const path = require("node:path")
 
 const checklistRouter = require("./src/routes/checklist")
 const rootRouter = require("./src/routes/index")
+
 require("./config/database")
 
 const app = express()
 app.use(express.json())
+
+app.use(express.static(path.join(__dirname, "public")))
 
 app.set("views", path.join(__dirname, "src/views"))
 app.set("view engine", "ejs")
